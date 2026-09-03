9e6946b3ce627d8561cf3b9ef079fa6fbf1c55c1
Kien Nguyen <hiimkien2702@gmail.com>
2026-09-03 10:46:24 -0400

    implemented blinking

---
+++ learning/ESP32/projects/01-blink/main/01-blink.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+
+#include "driver/gpio.h"
+
+#define BLINK_LED 2
+
+void app_main(void)
+{
+    char* taskName = pcTaskGetName(NULL);
+    ESP_LOGI(taskName, "Hello world!\n");
+
+    gpio_reset_pin(BLINK_LED);
+    gpio_set_direction(BLINK_LED, GPIO_MODE_OUTPUT);
+
+    while(1) {
+        gpio_set_level(BLINK_LED, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(BLINK_LED, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
